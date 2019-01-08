1d3a20ba5c11687fedebc49ca1be4fac1559b1fd
sergii <sergiishevchenko017@gmail.com>
2019-01-08 17:41:42 +0300

    2

--- entries/forms.py
+++ entries/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from .models import Entry
 
+
 class EntryForm(ModelForm):
     class Meta:
         model = Entry
--- entries/views.py
+++ entries/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect
 from .models import Entry
 from .forms import EntryForm
 
+
 def index(request):
     entries = Entry.objects.order_by('-date_posted')
 
     context = {'entries' : entries}
 
     return render(request, 'entries/index.html', context)
+
 
 def add(request):
     if request.method == 'POST':
